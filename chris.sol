d8ef88ad3a12e19a9a6c73bc206304e1e547feeb
Christopher Løkken <chrlokk1@gmail.com>
2021-11-15 12:45:06 +0100

    changed show dates to mapping between dates and mapping to seats by seat id

--- chris.sol
+++ chris.sol
@@ -4,14 +4,25 @@
 pragma solidity >=0.8.0;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
 
 contract TicketBookingSystem {
     mapping(string => Show) public shows;
     string[] showTitles;
+    Ticket ticket;
     
-    function buy (string memory _title, string memory _date, uint _numb, uint _row) public view returns (bool) {
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+    
+    constructor () public {
+        ticket = new Ticket();
+    }
+    
+    function buySeat (string memory _title, string memory _date, uint _numb, uint _row) public payable returns (bool) {
         Show show = shows[_title];
         if (show.canBuy(_date,_numb,_row)) {
+            bytes32 seatId = show.hash(_title,_date,_numb,_row);
+            ticket.mint(msg.sender, _tokenIds.current(), seatId, show);
             return true;
         }
         return false;
@@ -40,12 +51,13 @@ contract TicketBookingSystem {
 
 contract Show {
     string public title;
-    mapping(string=>Seat[]) public dates;
+    mapping(string=>mapping(bytes32=>Seat)) public dates; // {"2.des" -> {xyz:1, abc:2}},"3.des" -> ...}
     string[] dateIndex;
     uint private availableSeats;
     address public admin;
     
     struct Seat {
+        bytes32 id;
         string title;
         string date;
         uint price;
@@ -65,17 +77,21 @@ contract Show {
     
     function addDate (string memory _date) public {
         for (uint i = 0; i < availableSeats; i ++){
-            dates[_date].push(Seat(title, _date, 10, i, 1, "url:seat-link", false));
+            uint _price = 10;
+            uint _numb = i;
+            uint _row = 1;
+            bytes32 id = hash(title,_date,_numb,_row);
+            
+            dates[_date][id] = Seat(id, title, _date, _price, _numb, _row, "url:seat-link", false);
         }
         dateIndex.push(_date);
     }
     
     function canBuy (string memory _date, uint _numb, uint _row) public view returns(bool) {
-        Seat[] memory seats = dates[_date];
-        for (uint i = 0; i < availableSeats; i++) {
-            if (seats[i].numb == _numb && seats[i].row == _row && seats[i].booked == false) {
-                return true;
-            }
+        bytes32 seatId = hash(title,_date,_numb,_row);
+        Seat memory seat = dates[_date][seatId];
+        if (seat.booked == false) {
+            return true;
         }
         return false;
     }
@@ -87,6 +103,15 @@ contract Show {
     function getDates () public view returns(string[] memory) {
         return dateIndex;
     }
+    
+    function hash( // * slaps roof * "ripped this puppy of the good'ol web" -Me, 2021
+        string memory _title,
+        string memory _date,
+        uint _numb,
+        uint _row
+    ) public pure returns (bytes32) {
+        return keccak256(abi.encodePacked(_title, _date, _numb, _row));
+    }
 }
 
 
@@ -97,11 +122,12 @@ contract Poster is ERC721 {
 }
 
 contract Ticket is ERC721 {
-    address owner;
-    Show show;
-    constructor (address _owner, Show _show) ERC721 ("Ticket", "TKT") {
-        owner = _owner;
-        show = _show;
+    mapping(bytes32=>Show) tickets; // seat id --> show
+    constructor () ERC721 ("Ticket", "TKT") {}
+    
+    function mint(address _to, uint _ticketId, bytes32 _seatId, Show _show) public {
+        _safeMint(_to,_ticketId);
+        tickets[_seatId] = _show;
     }
 }
 
